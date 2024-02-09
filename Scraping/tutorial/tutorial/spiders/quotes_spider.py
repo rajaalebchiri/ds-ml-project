e1c663e0799084247d691c9854a621cb365102c9
rajaalebchiri <rajaa.lebchiri@gmail.com>
2024-02-09 23:24:38 +0100

    learn scraping

---
+++ Scraping/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import scrapy
+from tutorial.items import TutorialItem
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    # def start_requests(self):
+    #     urls = [
+    #         "https://quotes.toscrape.com/page/2/",
+    #         "https://quotes.toscrape.com/page/3/",
+    #         "https://quotes.toscrape.com/page/4/"
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    start_urls = [
+        "https://quotes.toscrape.com/page/2/",
+        "https://quotes.toscrape.com/page/3/",
+        "https://quotes.toscrape.com/page/4/"
+    ]
+
+    custom_settings = {
+        'FEEDS': {'data/data.json': {'format': 'json', 'overwrite': True}}
+    }
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            text = quote.css("span.text::text").get()
+            author = quote.css("small.author::text").get()
+            tags = quote.css("div.tags a.tag::text").getall()
+            tutorial_item = TutorialItem(
+                text=text, author=author, tags=tags
+            )
+            yield tutorial_item
